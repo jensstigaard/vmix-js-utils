8828882722e7cf06af72ed8b4ba1646f213716e8
Jens Stigaard <jensgs@bitbucket.org>
2018-03-04 01:31:55 +0100

    More explicit example of how to use the utils

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ const Connection = require('./src/connection')
 const StateFetcher = require('./src/state-fetcher')
 const InputMapper = require('./src/input-mapper')
 const ApiDataParser = require('./src/api-data-parser')
+const CommandSender = require('./src/command-sender')
 
-let connection = new Connection()
+// Connection - where is your vMix web controller located?
+let connection = new Connection('localhost', 8088)
+
 let stateFetcher = new StateFetcher(connection)
 
+// Register callback on success - when data was fetched, what to do with it?
 stateFetcher.registerCallbackOnSuccess(data => {
     let xmlContent = ApiDataParser.parse(data)
     let inputs = InputMapper.extractInputsFromXML(xmlContent)
     let inputsMap = InputMapper.mapInputs(inputs)
     let inputsList = Object.values(inputsMap)
 
-    console.log("YES", inputs.length, inputsList[0])
+    console.log("I did read from the vMix API! Inputs: ", inputs.length)
 })
 
-stateFetcher.registerCallbackOnError(data => {
-    console.error("Error..", data)
+stateFetcher.registerCallbackOnError(error => {
+    console.error(`Error.. Not able to read API data from a vMix web controller.. Trying again in ${stateFetcher.currentRefreshRate()}ms`)
+    //console.error(error)
 })
 
 stateFetcher.start()
+
+let commandSender = new CommandSender(connection)
+
+// Perform a command - Cut!
+commandSender.send({ Function: 'Cut' })
+    .catch(error => console.error('Could not perform cut'))
+    .then(response => console.log('Performed cut', response))
