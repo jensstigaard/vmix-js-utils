8266917dd7065d2b501198c71a3c59ec06f12053
Jens Stigaard <jensgs@bitbucket.org>
2018-03-04 11:49:39 +0100

    index.js now exposing modules (for npm)

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Examples can be found in the /examples folder
-require('./examples/state-fetcher-basic')
-require('./examples/command-sender-basic')
+
+// Exposing the util classes - used by npm
+module.exports = {
+    ApiDataParser: require('./src/api-data-parser'),
+    CommandSender: require('./src/command-sender'),
+    Connection: require('./src/connection'),
+    InputMapper: require('./src/input-mapper'),
+    StateFetcher: require('./src/state-fetcher'),
+}
