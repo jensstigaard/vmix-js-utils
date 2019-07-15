bb636d2f97ab53af3b5fd0ed721cc8d9aa5c30b3
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2019-07-15 16:14:45 +0200

    Temporarily disabled CommandSender and StateFetcher

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 // Exposing the util classes - used by npm
 module.exports = {
     ApiDataParser: require('./src/api-data-parser'),
-    CommandSender: require('./src/command-sender'),
-    Connection: require('./src/connection'),
+    // CommandSender: require('./src/command-sender'),
+    Connection: require('./src/connection-tcp'),
+    ConnectionHTTP: require('./src/connection-http'),
     InputMapper: require('./src/input-mapper'),
-    StateFetcher: require('./src/state-fetcher'),
+    // StateFetcher: require('./src/state-fetcher'),
 }
