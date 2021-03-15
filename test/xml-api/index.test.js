e96b16be8764c4c70ee129923cba12f9860a1466
Jens Stigaard <jensgs@bitbucket.org>
2021-03-15 18:48:54 +0100

    Added additional test

---
+++ test/xml-api/index.test.js
@@ -0,0 +1,17 @@
+// File system
+const fs = require('fs')
+const path = require('path')
+
+// Using assert library
+const assert = require('assert')
+
+// Import the modules
+const vMixApi = require('../../dist/modules/xml-api')
+const { XmlApi } = require('../../dist')
+
+describe('xml-api', function () {
+    it('it can be imported directly in main module and in submodule', function () {
+        assert.strictEqual(vMixApi, XmlApi)
+    })
+
+})
