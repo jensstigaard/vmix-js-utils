17fece63673cdafd69df0540c315d98ceaf2436d
Jens Grønhøj Stigaard <jens@stigaard.info>
2021-04-11 12:21:43 +0200

    Added mixin to let mapper include generic audio input attributes

--- src/modules/xml-api/input-mapping/video-input-mapper.ts
+++ src/modules/xml-api/input-mapping/video-input-mapper.ts
@@ -1,12 +1,15 @@
 
 import { PlayableInputMapper } from './playable-input-mapper'
 import { VideoInput } from '../../../types/input'
+import { GenericAudioInputMapper } from './generic-audio-input-mapper'
 
 export class VideoInputMapper extends PlayableInputMapper {
+
 	map(input: Element): VideoInput {
 		// Map base output attributes
 		const output = {
 			...super.map(input),
+			...(new GenericAudioInputMapper).map(input)
 		}
 
 		return output
