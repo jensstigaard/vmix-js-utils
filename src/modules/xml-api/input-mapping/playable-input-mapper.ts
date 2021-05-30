cec25a6de898f7e74c7c24152b903c83c3d6d857
Jens Grønhøj Stigaard <jens@stigaard.info>
2021-05-30 08:14:11 +0200

    Minor changes

--- src/modules/xml-api/input-mapping/playable-input-mapper.ts
+++ src/modules/xml-api/input-mapping/playable-input-mapper.ts
@@ -13,11 +13,15 @@ export abstract class PlayableInputMapper extends BaseInputMapper {
 
 			shortTitle: input.attributes.getNamedItem('shortTitle')!.value,
 			state: input.attributes.getNamedItem('state')!.value,
+			loop: input.attributes.getNamedItem('loop') ? Boolean(input.attributes.getNamedItem('loop')!.value === 'True') : false,
+
+			// Position and duration (in ms)
 			position: Number(input.attributes.getNamedItem('position')!.value),
 			duration: Number(input.attributes.getNamedItem('duration')!.value),
+
+			// Mark in / out point (in ms)
 			markIn: input.attributes.getNamedItem('markIn') ? Number(input.attributes.getNamedItem('markIn')!.value) : undefined,
 			markOut: input.attributes.getNamedItem('markOut') ? Number(input.attributes.getNamedItem('markOut')!.value) : undefined,
-			loop: input.attributes.getNamedItem('loop') ? Boolean(input.attributes.getNamedItem('loop')!.value === 'True') : false
 		}
 
 		return output
