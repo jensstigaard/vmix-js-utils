1bdcb0ab88821f5316c6ecec1627d7803573816e
hiddehs <hidde.schultze@gmail.com>
2022-01-08 19:16:58 +0100

    Add support for Capture inputs without AudioDevices

--- src/xml-api/input-mapping/generic-av-input-mapper.ts
+++ src/xml-api/input-mapping/generic-av-input-mapper.ts
@@ -1,15 +1,28 @@
 // Types
-import { VideoInput } from '../../types/inputs/video'
+import {VideoInput} from '../../types/inputs/video'
 // Mappers
-import { PlayableInputMapper } from './playable-input-mapper'
-import { GenericAudioInputMapper } from './generic-audio-input-mapper'
+import {PlayableInputMapper} from './playable-input-mapper'
+import {GenericAudioInputMapper} from './generic-audio-input-mapper'
 
 export abstract class GenericPlayableWithAudioInputMapper extends PlayableInputMapper {
 
-	map(input: Element, includeLayers: boolean = true): VideoInput {
-		return {
-			...super.map(input, includeLayers),
-			...(new GenericAudioInputMapper).map(input)
-		}
-	}
+  map(input: Element, includeLayers: boolean = true): VideoInput {
+    if (input.attributes.getNamedItem('audiobusses')) {
+      return {
+        ...super.map(input, includeLayers),
+        ...(new GenericAudioInputMapper).map(input)
+      }
+    } else {
+      return {
+        ...super.map(input, includeLayers),
+        audiobusses: [],
+        muted: false,
+        solo: false,
+        volume: -1,
+        balance: 0,
+        audioMeter: {left: 0, right: 0},
+        gainDb: 0
+      }
+    }
+  }
 }
